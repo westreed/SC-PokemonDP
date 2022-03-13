dffe7bce48ed026200185985084efc2547eec5af
JangSeHun <westreed@naver.com>
2022-03-13 22:05:30 +0900

    아이템 추가 및 고질적인 EUD ERROR 고침

--- BulidData_포켓몬스터DP/eudplibData/TriggerEditor/PluginVariables.py
+++ BulidData_포켓몬스터DP/eudplibData/TriggerEditor/PluginVariables.py
@@ -3,9 +3,11 @@ import TriggerEditor.SCArchive as sca
 
 
 VChatIndex = EUDArray(8)
+VMouseDown_L = EUDArray(8)
 
 def Reg():
     print('... TERegVar ...')
+    EUDRegisterObjectToNamespace("VMouseDown_L", VMouseDown_L)
     EUDRegisterObjectToNamespace("VChatIndex", VChatIndex)
     sca.Init()
 EUDOnStart(Reg)
